5b4734324fbb75533084e83facb55801dd1ca491
rabit <lj79490324@gmail.com>
2017-10-13 14:57:37 +0800

    init commit

---
+++ src/main/java/com/rabit/Sun.java
@@ -0,0 +1,47 @@
+package com.rabit;
+
+public class Sun {
+    private int id;
+    private int age;
+    private String name;
+    private String desc;
+
+    public Sun(int id, int age, String name, String desc) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
